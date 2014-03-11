fa4b5687d2aab4fc6fdaf72c7b1416eb668fc2a6
Elliott Barrere  <elliott@continuum.us>
2014-03-11 09:56:19 -0700

    Add lead() method to return username of component lead

--- lib/jira/resource/component.rb
+++ lib/jira/resource/component.rb
@@ -4,7 +4,14 @@ module JIRA
     class ComponentFactory < JIRA::BaseFactory # :nodoc:
     end
 
-    class Component < JIRA::Base ; end
+    class Component < JIRA::Base
+      def lead()
+        url = client.options[:rest_base_path] + '/component/' + self.id
+        response = client.get(url)
+        json = self.class.parse_json(response.body)
+        json["realAssignee"] ? json["realAssignee"]["name"] : nil
+      end
+    end
 
   end
 end
